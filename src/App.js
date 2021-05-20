a18cba1e43179ff10d0591939eafdcae82738cfc
Amber Lee <lee.amber.alex@gmail.com>
2021-05-19 20:00:36 -0700

    Updated browser to fix deploy.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import bg2 from "./img/bg2.jpg";
 import "./App.css";
 import Nav from "./nav/Nav";
 // import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div style={{ backgroundImage: `url(${bg2})` }}>
-      <Router basename="/lee.amber.alex">
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={PortHome} />
-          <Route path= "/projects" component={ProjectContainer} />
-        </Switch>
-      </Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <PortHome/>
+        </Route>
+        <Route path="/projects">
+          <ProjectContainer/>
+        </Route>
+        {/* <Route exact path="/" component={PortHome} /> */}
+        {/* <Route path="/projects" component={ProjectContainer} /> */}
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import { BrowserRouter as Router } from "react-router-dom"; 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { HashRouter} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById("root"));
