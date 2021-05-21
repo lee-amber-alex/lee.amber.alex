b0fb6951d2e076134537ffef949d293f75f85d23
Amber Lee <lee.amber.alex@gmail.com>
2021-05-21 10:31:35 -0700

    Updated skills.

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,7 @@ import "../style/porthome.css"
 export default function Bio() {
     return (
         <div>
-            <img className="me-bio p-2" src={me} alt="AmberLee" />
+            <img className="me-bio " src={me} alt="AmberLee" />
             <i><b className="about-me">Creative Front End Web Developer </b></i>and experienced Customer Service professional who values
             teamwork and collaboration. Her client-facing expertise encompasses visual effects production coordination
             at Lucasfilm, as well as, local business outreach for the Print and Marketing division at Staples. <br/>
@@ -13,7 +13,7 @@ export default function Bio() {
             continue to be creative, apply customer service skills, work with a team and better support her family.
             Having an interest in technical space while at Lucasfilm, she naturally gravitated back to software and
             achieved a Full-Stack Web Development certificate from the University of Washington. Her technical skills
-            now include: CSS, HTML, JavaScript, jQuery, MySQL, MongoDB, Node.js, Express.js, and React. AWS Certified Cloud Practitioner.<br/><br/>
+            now include: AWS Certified Cloud Practitioner, CSS, HTML, JavaScript, jQuery, TypeScript, MySQL, MongoDB, Node.js, Express.js, and React. <br/><br/>
 
             With a newly found enthusiasm for coding, Amber now has the ability to apply aspects of UX and agile
             development to build single-page MERN applications that help communities connect and businesses thrive. She
